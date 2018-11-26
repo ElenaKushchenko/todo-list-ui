a4275824dd12c7c822a18a63582f6ac27f8b3165
Elena_Kushchenko <Elena_Kushchenko@epam.com>
2018-11-27 01:52:51 +0300

    layout improvements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {MaterialModule} from "./material/material.module";
-import { HeaderComponent } from './header/header.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ProjectListComponent } from './project-list/project-list.component';
+import {HeaderComponent} from './header/header.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {ProjectListComponent} from './project-list/project-list.component';
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { ProjectListComponent } from './project-list/project-list.component';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    FlexLayoutModule,
     MaterialModule
   ],
   providers: [],

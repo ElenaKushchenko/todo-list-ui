ee4d77c30e974a320e057f1df7fe6d9488e12bce
Elena_Kushchenko <Elena_Kushchenko@epam.com>
2018-11-29 02:59:11 +0300

    implemented logic of task dialog window

--- src/app/dialog/task-dialog/task-dialog.component.ts
+++ src/app/dialog/task-dialog/task-dialog.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
+import {Task} from "../../model/task";
 
 @Component({
   selector: 'app-task-dialog',
   templateUrl: './task-dialog.component.html',
   styleUrls: ['./task-dialog.component.scss']
 })
-export class TaskDialogComponent implements OnInit {
+export class TaskDialogComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(public dialogRef: MatDialogRef<TaskDialogComponent>,
+              @Inject(MAT_DIALOG_DATA)
+              public task?: Task) {
   }
 
+  onClose(task?: Task) {
+    this.dialogRef.close(task)
+  }
 }
